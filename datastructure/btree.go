7d1c18c1e2b6e5a16141d882eb2c132027b19a0f
MaybeSilent <804758962@qq.com>
2021-04-30 16:37:30 +0800

    add go mod

--- datastructure/btree.go
+++ datastructure/btree.go
@@ -9,3 +9,12 @@ type BTree struct {
 	mu sync.RWMutex	// read and write lock for cow node root change
 	tree *btree.BTree	//
 }
+// 测试BTree
+func NewBTree() {
+
+}
+
+//
+func Iterator() {
+
+}
---
+++ datastructure/go.mod
@@ -0,0 +1,5 @@
+module github.com/maybesilent/datastructure
+
+go 1.15
+
+require github.com/google/btree v1.0.1
