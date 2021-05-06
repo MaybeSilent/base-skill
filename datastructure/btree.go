c46ceb331c8567e528a8effff883d8984e83ae8d
MaybeSilent <804758962@qq.com>
2021-05-06 21:53:53 +0800

    add useage of cow btree

--- datastructure/btree.go
+++ datastructure/btree.go
@@ -2,6 +2,7 @@ package datastructure
 
 import (
 	"sync"
+	"sync/atomic"
 
 	"github.com/google/btree"
 )
@@ -9,27 +10,36 @@ import (
 // aim to use cow on google's btree to avoid data race
 type BTree struct {
 	mu   *sync.RWMutex // read and write lock for cow node root change
-	tree *btree.BTree  // btree of google
+	tree atomic.Value  // btree of google
 }
 
 // new wrapper btree
 func NewBTree(degree int) *BTree {
-	return &BTree{
-		mu:   &sync.RWMutex{},
-		tree: btree.New(degree),
+	res := &BTree{
+		mu: &sync.RWMutex{},
 	}
+	res.tree.Store(btree.New(degree))
+	return res
 }
 
 // insert or update the item in btree
 func (b *BTree) InsertOrUpdate(item btree.Item) {
-	b.tree.ReplaceOrInsert(item)
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	cloneTree := b.tree.Load().(*btree.BTree).Clone()
+	cloneTree.ReplaceOrInsert(item)
+	b.tree.Store(cloneTree)
 }
 
 func (b *BTree) Delete(item btree.Item) {
-	b.tree.Delete(item)
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	cloneTree := b.tree.Load().(*btree.BTree).Clone()
+	cloneTree.ReplaceOrInsert(item)
+	b.tree.Store(cloneTree)
 }
 
 // iterate the btree
 func (b *BTree) Iterator(iterator btree.ItemIterator) {
-	b.tree.Ascend(iterator)
+	b.tree.Load().(*btree.BTree).Ascend(iterator)
 }
--- datastructure/btree_main.go
+++ datastructure/btree_main.go
@@ -15,9 +15,17 @@ func main() {
 	tree := datastructure.NewBTree(8)
 	list := rand.Perm(10) // 生成rand的随机序列
 
+	deleteList := rand.Perm(5) // 生成delete的随机序列
+
 	go func() {
 		for _, num := range list {
 			tree.InsertOrUpdate(btree.Int(num))
+		}
+	}()
+
+	go func() {
+		for _, num := range deleteList {
+			tree.Delete(btree.Int(num))
 		}
 	}()
 
