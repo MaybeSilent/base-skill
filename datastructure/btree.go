d516b187c916a00675fa0ef7c6bcdd298c24ad4b
MaybeSilent <804758962@qq.com>
2021-05-06 20:55:57 +0800

    add basic operation

--- datastructure/btree.go
+++ datastructure/btree.go
@@ -1,20 +1,35 @@
 package datastructure
 
 import (
-	"github.com/google/btree"
 	"sync"
+
+	"github.com/google/btree"
 )
 
+// aim to use cow on google's btree to avoid data race
 type BTree struct {
-	mu sync.RWMutex	// read and write lock for cow node root change
-	tree *btree.BTree	//
-}
-// 测试BTree
-func NewBTree() {
-
+	mu   *sync.RWMutex // read and write lock for cow node root change
+	tree *btree.BTree  // btree of google
 }
 
-//
-func Iterator() {
+// new wrapper btree
+func NewBTree(degree int) *BTree {
+	return &BTree{
+		mu:   &sync.RWMutex{},
+		tree: btree.New(degree),
+	}
+}
 
+// insert or update the item in btree
+func (b *BTree) InsertOrUpdate(item btree.Item) {
+	b.tree.ReplaceOrInsert(item)
+}
+
+func (b *BTree) Delete(item btree.Item) {
+	b.tree.Delete(item)
+}
+
+// iterate the btree
+func (b *BTree) Iterator(iterator btree.ItemIterator) {
+	b.tree.Ascend(iterator)
 }
--- datastructure/btree_main.go
+++ datastructure/btree_main.go
@@ -3,6 +3,25 @@
 
 package main
 
-func main() {
+import (
+	"math/rand"
 
+	"github.com/google/btree"
+
+	"github.com/maybesilent/datastructure"
+)
+
+func main() {
+	tree := datastructure.NewBTree(8)
+	list := rand.Perm(10) // 生成rand的随机序列
+
+	go func() {
+		for _, num := range list {
+			tree.InsertOrUpdate(btree.Int(num))
+		}
+	}()
+
+	tree.Iterator(func(i btree.Item) bool {
+		return true
+	})
 }
