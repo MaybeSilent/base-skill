99c49c790e72c7b4df021a46661583816a28697c
MaybeSilent <804758962@qq.com>
2021-05-11 10:31:05 +0800

    change style

--- leetcode/5.java
+++ leetcode/5.java
@@ -67,13 +67,13 @@ class Solution {
         char[] strs = getCharArray(s);
         int maxMid = 0, maxRight = 0;
         int[] value = new int[strs.length];
-        for (int i = 1; i < strs.length - 1; i ++) {
+        for (int i = 1; i < strs.length - 1; i++) {
             int length = 1;
             if (i < maxRight) {
                 length = Math.min(value[maxMid * 2 - i], maxRight - i);
             }
-            while (i + length < strs.length && i - length >= 0 && strs[i+length] == strs[i-length]) {
-                length ++;
+            while (i + length < strs.length && i - length >= 0 && strs[i + length] == strs[i - length]) {
+                length++;
             }
             value[i] = length - 1;
             if (i + value[i] > maxRight) {
@@ -82,7 +82,7 @@ class Solution {
             }
         }
         int max = 0;
-        for (int i = 0; i < strs.length - 1; i ++) {
+        for (int i = 0; i < strs.length - 1; i++) {
             if (value[i] > value[max] || (value[i] == value[max] && (i - value[i]) % 2 != 0)) {
                 max = i;
             }
@@ -92,7 +92,7 @@ class Solution {
         }
 
         StringBuffer sb = new StringBuffer();
-        for (int i = max - value[max]; i <= max + value[max]; i ++) {
+        for (int i = max - value[max]; i <= max + value[max]; i++) {
             if (strs[i] != '#') {
                 sb.append(strs[i]);
             }
@@ -102,11 +102,12 @@ class Solution {
 
     private char[] getCharArray(String s) {
         char[] result = new char[s.length() * 2 + 1];
-        for (int i = 0; i < s.length(); i ++) {
-            result[i*2] = '#';
-            result[i*2+1] = s.charAt(i);
+        for (int i = 0; i < s.length(); i++) {
+            result[i * 2] = '#';
+            result[i * 2 + 1] = s.charAt(i);
         }
-        result[0] = '$'; result[result.length - 1] = '^';
+        result[0] = '$';
+        result[result.length - 1] = '^';
         return result;
     }
 
@@ -119,15 +120,15 @@ class Solution {
         String result = s.substring(0, 1);
         char[] strs = s.toCharArray();
 
-        for (int i = 0; i < s.length(); i ++) {
+        for (int i = 0; i < s.length(); i++) {
             dp[i][i] = true;
         }
 
-        for (int j = 1; j < s.length(); j ++) {
-            for (int i = 0; i < j; i ++) {
-                dp[i][j] = j == i + 1  ? strs[i] == strs[j] : dp[i+1][j-1] && (strs[i] == strs[j]);
+        for (int j = 1; j < s.length(); j++) {
+            for (int i = 0; i < j; i++) {
+                dp[i][j] = j == i + 1 ? strs[i] == strs[j] : dp[i + 1][j - 1] && (strs[i] == strs[j]);
                 if (dp[i][j] && j - i + 1 > result.length()) {
-                    result = s.substring(i, j+1);
+                    result = s.substring(i, j + 1);
                 }
             }
         }
@@ -135,37 +136,36 @@ class Solution {
         return result;
     }
 
-
     private String forLoopPalindrome(String s) {
         int max = 0;
         char[] strs = s.toCharArray();
         String result = new String();
-        for (int i = 0; i < strs.length; i ++) {
+        for (int i = 0; i < strs.length; i++) {
             // 奇数的情况
             int index = 0;
-            for (index = 1; i + index < strs.length && i - index >= 0; index ++) {
+            for (index = 1; i + index < strs.length && i - index >= 0; index++) {
                 if (strs[i - index] != strs[i + index]) {
                     break;
                 }
             }
-            index --;
+            index--;
             if (index * 2 + 1 > max) {
                 max = index * 2 + 1;
                 result = s.substring(i - index, i + index + 1);
             }
 
             // 偶数的情况
-            if (i + 1 < strs.length && strs[i+1] == strs[i]) {
+            if (i + 1 < strs.length && strs[i + 1] == strs[i]) {
                 int j = i + 1;
                 index = 1;
                 while (i - index >= 0 && j + index < strs.length) {
                     if (strs[i - index] != strs[j + index]) {
                         break;
                     }
-                    index ++;
+                    index++;
                 }
-                index --;
-                if (index*2 + 2 > max) {
+                index--;
+                if (index * 2 + 2 > max) {
                     max = index * 2 + 2;
                     result = s.substring(i - index, j + index + 1);
                 }
@@ -175,4 +175,3 @@ class Solution {
     }
 }
 // @lc code=end
-
