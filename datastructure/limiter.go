619a16487d4f743f5e41e64c16908d7bb323a171
MaybeSilent <804758962@qq.com>
2021-06-04 11:30:00 +0800

    update limit

--- datastructure/limiter.go
+++ datastructure/limiter.go
@@ -7,15 +7,24 @@ import (
 
 type Limiter struct {
 	QPS       int32
-	Remaining int32 // 剩余的可以通过的数量
+	Remaining int32         // 剩余的可以通过的数量
+	Interval  time.Duration // 每次更新限流器的时间间隔
 }
 
-func NewLimiter(QPS int) *Limiter {
+func NewLimiter(QPS int, refreshInterval time.Duration) *Limiter {
 	if QPS < 0 {
 		QPS = 0
 	}
+	// 限流器时间设置
+	if refreshInterval < time.Millisecond*10 {
+		refreshInterval = time.Millisecond * 10
+	}
+	if refreshInterval > time.Second {
+		refreshInterval = time.Second
+	}
 	res := &Limiter{
-		QPS: int32(QPS),
+		QPS:      int32(QPS),
+		Interval: refreshInterval,
 	}
 	go func() {
 		res.refresh()
@@ -25,15 +34,15 @@ func NewLimiter(QPS int) *Limiter {
 
 func (l *Limiter) refresh() {
 	// 更新时间根据
-	addInt := l.QPS / 10
+	addInt := l.QPS / int32(time.Second/l.Interval)
 	if addInt == 0 && !(l.QPS == 0) {
 		addInt = 1
 	}
 
-	addTicker := time.Tick(time.Millisecond * 100)
+	addTicker := time.Tick(l.Interval)
 	for {
-		<-addTicker //
-		if atomic.LoadInt32(&l.Remaining) > l.QPS {
+		<-addTicker // 根据l.Interval进行触发
+		if atomic.LoadInt32(&l.Remaining)+addInt > l.QPS {
 			continue
 		}
 		atomic.AddInt32(&l.Remaining, addInt)
@@ -41,9 +50,8 @@ func (l *Limiter) refresh() {
 }
 
 func (l *Limiter) Pass() bool {
-	if atomic.LoadInt32(&l.Remaining) > 0 {
-		atomic.AddInt32(&l.Remaining, -1)
-		return true
+	if atomic.LoadInt32(&l.Remaining) > 0 { // 并发扣减分开，会有击穿的风险
+		return atomic.AddInt32(&l.Remaining, -1) >= 0
 	}
 	return false
 }
