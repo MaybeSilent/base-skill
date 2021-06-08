85f7e106eefed090f9686e1c7a9cc55ba9ed2a7d
MaybeSilent <804758962@qq.com>
2021-06-08 21:09:48 +0800

    add 33

--- leetcode/33.java
+++ leetcode/33.java
@@ -66,10 +66,49 @@ class Solution {
     public int search(int[] nums, int target) {
         // 先利用二分查找，找出旋转点
         // 再根据旋转点为边界，进行二分查找
+        if (nums.length == 0)
+            return -1;
+        int split = getNumsIndex(nums);
+        int left = getTarget(nums, 0, split - 1, target);
+        if (left != -1)
+            return left;
+        int right = getTarget(nums, split, nums.length - 1, target);
+        return right;
+    }
+
+    private int getTarget(int[] nums, int start, int end, int target) {
+        if (start > end) {
+            return -1;
+        }
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > target) {
+                end = mid - 1;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 
     private int getNumsIndex(int[] nums) {
-        
+        if (nums[0] < nums[nums.length - 1]) {
+            return 0;
+        }
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[start]) {
+                start = mid;
+            } else if (nums[mid] < nums[start]) {
+                end = mid;
+            } else {
+                return end;
+            }
+        }
+        return end;
     }
 }
 // @lc code=end
