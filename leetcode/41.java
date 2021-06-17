7964739491a3d46ee4d42ca10a0c59f4b0e5a971
MaybeSilent <804758962@qq.com>
2021-06-17 21:45:28 +0800

    add 41

--- leetcode/41.java
+++ leetcode/41.java
@@ -53,8 +53,24 @@
 // @lc code=start
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (i == nums[i] - 1)
+                continue;
+            while (i != nums[i] - 1) {
+                if (nums[i] - 1 >= nums.length || nums[i] - 1 < 0 || nums[nums[i] - 1] == nums[i]) {
+                    break;
+                }
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
 
+        int res = 0;
+        while (res < nums.length && res == nums[res] - 1) {
+            res++;
+        }
+        return res + 1;
     }
 }
 // @lc code=end
-
