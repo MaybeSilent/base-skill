ad288997540d9014684f747fafbc3bd613775a9a
MaybeSilent <804758962@qq.com>
2021-06-17 22:09:07 +0800

    add 42

--- leetcode/42.java
+++ leetcode/42.java
@@ -49,8 +49,37 @@
 // @lc code=start
 class Solution {
     public int trap(int[] height) {
+        int[] leftMax = new int[height.length];
+        int[] rightMax = new int[height.length];
+        for (int i = 1; i < height.length; i++) {
+            leftMax[i] = Math.max(height[i - 1], leftMax[i - 1]);
+        }
 
+        for (int i = height.length - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i + 1]);
+        }
+
+        // for (int i = 0; i < leftMax.length; i++) {
+        // System.out.print(leftMax[i] + " ");
+        // }
+
+        // System.out.println();
+
+        // for (int i = 0; i < rightMax.length; i++) {
+        // System.out.print(rightMax[i] + " ");
+        // }
+
+        // System.out.println();
+
+        int res = 0;
+        for (int i = 0; i < height.length; i++) {
+            // System.out.print(res + " ");
+            int val = Math.min(leftMax[i], rightMax[i]) - height[i];
+            if (val > 0) {
+                res += val;
+            }
+        }
+        return res;
     }
 }
 // @lc code=end
-
