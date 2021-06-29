6829014f9040070dcf2cfa11d0de3d0ee158893c
MaybeSilent <804758962@qq.com>
2021-06-29 20:41:10 +0800

    add insert in rb_tree

--- datastructure/rb_tree.go
+++ datastructure/rb_tree.go
@@ -5,13 +5,16 @@ type Key interface {
 	LessThan(interface{}) bool
 }
 
-type value interface{}
+type Value interface{}
+
+const Red = 1
+const Black = 0
 
 type node struct {
 	left, right, parent *node
 	color               int
 	key                 Key
-	value               value
+	value               Value
 }
 
 // 红黑树的定义
@@ -24,7 +27,7 @@ type Tree struct {
 func (t *Tree) findNode(key Key) *node {
 	x := t.root
 	for x != nil {
-		if key.LessThan(t.root) {
+		if key.LessThan(t.root.key) {
 			x = x.left
 		} else {
 			if key == x.key {
@@ -37,8 +40,72 @@ func (t *Tree) findNode(key Key) *node {
 }
 
 // 红黑树插入的代码
-func (t *Tree) Insert() {
+func (t *Tree) Insert(key Key, value Value) {
+	x := t.root
 
+	var parentNode *node
+	for x != nil {
+		parentNode = x
+		if key.LessThan(x.key) {
+			x = x.left
+		} else {
+			x = x.right
+		}
+	}
+
+	insertNode := &node{key: key, value: value, parent: parentNode, color: Red}
+	if parentNode == nil {
+		t.root = insertNode
+		insertNode.color = Black
+		return
+	} else if key.LessThan(parentNode.key) {
+		parentNode.left = insertNode
+	} else {
+		parentNode.right = insertNode
+	}
+
+	// 调整树的整体结构
+	t.fixTreeWhenInsert(insertNode)
+}
+
+// 插入时进行结构调整
+func (t *Tree) fixTreeWhenInsert(insert *node) {
+	// 插入的节点为红色，可能会出现两个红色节点在一起的情况，违反了相关性质
+	if insert.parent != nil && insert.parent.color == Red {
+		if insert.parent == insert.parent.parent.left {
+			uncleNode := insert.parent.parent.right
+			if uncleNode != nil && uncleNode.color == Red { // 父节点与叔叔节点都为红色的情况
+				insert.parent.color, uncleNode.color = Black, Black
+				insert.parent.parent.color = Red
+				insert = insert.parent.parent
+			} else { // 叔叔节点为黑色的情况下处理
+				if insert == insert.parent.right { // 对于需要
+					insert = insert.parent
+					t.leftRotate(insert)
+				}
+				insert.parent.color = Black
+				insert.parent.parent.color = Red
+				t.rightRotate(insert.parent.parent)
+			}
+		} else { // 属于右节点的情况，对称操作
+			uncleNode := insert.parent.parent.left
+			if uncleNode != nil && uncleNode.color == Red { // 叔叔节点为红色的情况
+				insert.parent.color, uncleNode.color = Black, Black
+				insert.parent.parent.color = Red
+				insert = insert.parent.parent
+			} else { // 叔叔节点为黑色的情况处理
+				if insert == insert.parent.left { //
+					insert = insert.parent
+					t.rightRotate(insert)
+				}
+				insert.parent.color = Black
+				insert.parent.parent.color = Red
+				t.leftRotate(insert.parent.parent)
+			}
+		}
+	}
+
+	t.root.color = Black
 }
 
 // 红黑树删除的代码
@@ -93,7 +160,7 @@ func (t *Tree) rightRotate(x *node) {
 // 查找后继者的代码，查找node的后继者（值）
 func successor(node *node) *node {
 	if node.right != nil {
-		return minimun(node)
+		return minimum(node)
 	}
 
 	x := node.parent
@@ -104,7 +171,7 @@ func successor(node *node) *node {
 	return x
 }
 
-func minimun(node *node) *node {
+func minimum(node *node) *node {
 	for node.left != nil {
 		node = node.left
 	}
