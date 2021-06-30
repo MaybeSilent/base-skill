79ce3421292c3eb8525e45b2e6009c2e5ee06d88
MaybeSilent <804758962@qq.com>
2021-06-30 11:40:11 +0800

    add delete of rb_tree

--- datastructure/rb_tree.go
+++ datastructure/rb_tree.go
@@ -109,8 +109,119 @@ func (t *Tree) fixTreeWhenInsert(insert *node) {
 }
 
 // 红黑树删除的代码
-func (t *Tree) Delete() {
+func (t *Tree) Delete(key Key) {
+	keyNode := t.findNode(key)
+	if keyNode == nil {
+		return
+	}
 
+	var deleteNode, nextNode *node
+	if keyNode.left != nil && keyNode.right != nil {
+		deleteNode = successor(keyNode)
+		keyNode.key = deleteNode.key     // 利用后继节点替换当前节点
+		keyNode.value = deleteNode.value // key，value进行替换
+	} else {
+		deleteNode = keyNode
+	}
+
+	if deleteNode.left != nil {
+		nextNode = deleteNode.left
+	} else {
+		nextNode = deleteNode.right
+	}
+
+	parentNode := deleteNode.parent
+	if nextNode != nil {
+		nextNode.parent = parentNode // 将下一个节点设置为
+	}
+	// 设置相应的parent
+	if parentNode == nil {
+		t.root = nextNode // 树的跟节点为nextNode
+	} else if deleteNode == parentNode.left {
+		parentNode.left = nextNode
+	} else {
+		parentNode.right = nextNode
+	}
+
+	if deleteNode.color == Black { // 红黑树中删除红色节点并不会改变节点性质
+		t.fixTreeWhenDelete(nextNode, parentNode)
+	}
+
+	t.size-- // 树的大小 -1
+}
+
+// 进行删除节点的树结构调整
+func (t *Tree) fixTreeWhenDelete(newNode, parent *node) {
+	// 删除节点会破坏相关树的结构
+	for newNode != t.root && getColor(newNode) == Black { // 替代的节点为黑色的情况，节点路径上缺少了黑色的节点
+		if newNode != nil {
+			parent = newNode.parent
+		}
+
+		if newNode == parent.left {
+			uncleNode := parent.right
+			if uncleNode.color == Red {
+				uncleNode.color = Black
+				parent.color = Red
+				t.leftRotate(parent)
+				uncleNode = parent.right
+			}
+
+			if getColor(uncleNode.left) == Black && getColor(uncleNode.right) == Black {
+				uncleNode.color = Red
+				newNode = parent
+			} else { // 红色节点一定为有颜色的节点，并不会为nil
+				if getColor(uncleNode.right) == Black {
+					uncleNode.left.color = Black
+					uncleNode.color = Red
+					t.rightRotate(uncleNode)
+					uncleNode = parent.right
+				}
+				// 红黑树右节点为红色的情况
+				uncleNode.color = parent.color
+				parent.color, uncleNode.right.color = Black, Black
+				t.leftRotate(parent)
+				newNode = t.root // 结束循环
+			}
+		} else { // 删除的黑色节点在父节点的右子树
+			uncleNode := parent.left // x本身为黑色，uncle节点不可能为nil
+			if uncleNode.color == Red {
+				uncleNode.color = Black
+				parent.color = Red
+				t.rightRotate(parent)
+				uncleNode = parent.left
+			}
+
+			if getColor(uncleNode.left) == Black && getColor(uncleNode.right) == Black {
+				uncleNode.color = Red
+				newNode = parent
+			} else {
+				if getColor(uncleNode.left) == Black {
+					uncleNode.right.color = Black
+					uncleNode.color = Red
+					t.leftRotate(uncleNode)
+					uncleNode = parent.left
+				}
+				// 进行右旋调整树结构
+				uncleNode.color = parent.color
+				parent.color, uncleNode.left.color = Black, Black
+				t.rightRotate(parent)
+				newNode = t.root
+			}
+		}
+	}
+
+	if newNode != nil { // 根结点或者替代的子节点为红色，直接将相关的子节点置为黑色
+		newNode.color = Black
+	}
+}
+
+// 获取节点颜色
+func getColor(x *node) int {
+	if x == nil {
+		return Black
+	}
+	return x.color
 }
 
 // 红黑树的左旋与右旋
