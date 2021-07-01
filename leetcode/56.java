ca794e381d33ef3a4266dc039aaf4bb57f5a395b
MaybeSilent <804758962@qq.com>
2021-07-02 07:58:47 +0800

    add 56

---
+++ leetcode/56.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+ * @lc app=leetcode.cn id=56 lang=java
+ *
+ * [56] 合并区间
+ *
+ * https://leetcode-cn.com/problems/merge-intervals/description/
+ *
+ * algorithms
+ * Medium (45.57%)
+ * Likes:    988
+ * Dislikes: 0
+ * Total Accepted:    252.6K
+ * Total Submissions: 549.3K
+ * Testcase Example:  '[[1,3],[2,6],[8,10],[15,18]]'
+ *
+ * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi]
+ * 。请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * 输出：[[1,6],[8,10],[15,18]]
+ * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：intervals = [[1,4],[4,5]]
+ * 输出：[[1,5]]
+ * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * intervals[i].length == 2
+ * 0 i i 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    // 先排序，再在排序的基础上进行区间的合并
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length < 2) {
+            return intervals;
+        }
+
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        ArrayList<int[]> list = new ArrayList<>();
+        int[] prev = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] > prev[1]) {
+                list.add(prev);
+                prev = intervals[i];
+            } else {
+                prev = new int[] { prev[0], Math.max(prev[1], intervals[i][1]) };
+            }
+        }
+        list.add(prev);
+
+        int[][] res = new int[list.size()][2];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+
+        return res;
+    }
+}
+// @lc code=end
