9cfbb4bfe2c51df21982421dfda14d9f29ac2183
MaybeSilent <804758962@qq.com>
2021-07-06 17:42:37 +0800

    add 64

--- leetcode/64.java
+++ leetcode/64.java
@@ -50,8 +50,22 @@
 // @lc code=start
 class Solution {
     public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] min = new int[n];
 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    min[j] = min[j] + grid[i][j];
+                } else if (i == 0) {
+                    min[j] = min[j - 1] + grid[i][j];
+                } else {
+                    min[j] = Math.min(min[j - 1], min[j]) + grid[i][j];
+                }
+            }
+        }
+        return min[n - 1];
     }
 }
 // @lc code=end
-
