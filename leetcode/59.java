0917cf405dcbe9382c181bf097f409ef2c227e5f
MaybeSilent <804758962@qq.com>
2021-07-06 10:19:31 +0800

    add 58-65

---
+++ leetcode/59.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=59 lang=java
+ *
+ * [59] 螺旋矩阵 II
+ *
+ * https://leetcode-cn.com/problems/spiral-matrix-ii/description/
+ *
+ * algorithms
+ * Medium (79.64%)
+ * Likes:    434
+ * Dislikes: 0
+ * Total Accepted:    109K
+ * Total Submissions: 136.9K
+ * Testcase Example:  '3'
+ *
+ * 给你一个正整数 n ，生成一个包含 1 到 n^2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：n = 3
+ * 输出：[[1,2,3],[8,9,4],[7,6,5]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：n = 1
+ * 输出：[[1]]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] generateMatrix(int n) {
+
+    }
+}
+// @lc code=end
+
---
+++ leetcode/61.java
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=61 lang=java
+ *
+ * [61] 旋转链表
+ *
+ * https://leetcode-cn.com/problems/rotate-list/description/
+ *
+ * algorithms
+ * Medium (41.81%)
+ * Likes:    585
+ * Dislikes: 0
+ * Total Accepted:    177.3K
+ * Total Submissions: 424K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,3,4,5], k = 2
+ * 输出：[4,5,1,2,3]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [0,1,2], k = 4
+ * 输出：[2,0,1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点的数目在范围 [0, 500] 内
+ * -100 
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+
+    }
+}
+// @lc code=end
+
