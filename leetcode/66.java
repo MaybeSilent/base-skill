17f0ae17114a58ab6230749e2479e9e39e303afe
MaybeSilent <804758962@qq.com>
2021-07-06 21:12:05 +0800

    add 66

--- leetcode/66.java
+++ leetcode/66.java
@@ -58,8 +58,24 @@
 // @lc code=start
 class Solution {
     public int[] plusOne(int[] digits) {
+        if (digits.length == 0) {
+            return digits;
+        }
 
+        digits[digits.length - 1] += 1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] >= 10) {
+                digits[i] = digits[i] % 10;
+                if (i == 0) {
+                    int[] res = new int[digits.length + 1];
+                    res[0] = 1;
+                    return res;
+                } else {
+                    digits[i - 1] += 1;
+                }
+            }
+        }
+        return digits;
     }
 }
 // @lc code=end
-
