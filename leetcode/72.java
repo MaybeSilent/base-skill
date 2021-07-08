0eb00adf5cc949587385581e66408be9d04504f9
MaybeSilent <804758962@qq.com>
2021-07-08 09:33:18 +0800

    add 72

--- leetcode/72.java
+++ leetcode/72.java
@@ -63,8 +63,41 @@
 // @lc code=start
 class Solution {
     public int minDistance(String word1, String word2) {
+        // 动态规划相关的单词编辑总数
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        char[] word1Str = word1.toCharArray();
+        char[] word2Str = word2.toCharArray();
+        // word1需要删除字母
+        for (int i = 0; i <= word1.length(); i++) {
+            dp[i][0] = i;
+        }
+        // word1需要增加字母
+        for (int i = 0; i <= word2.length(); i++) {
+            dp[0][i] = i;
+        }
 
+        for (int i = 1; i <= word1.length(); i++) {
+            // 将word1转化为word2
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1Str[i - 1] == word2Str[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    // dp[i-1][j] 删除word1
+                    // dp[i-1][j-1] 修改word1
+                    // dp[i][j-1] 增加word1
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i - 1][j - 1]), dp[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        // for (int i = 0; i <= word1.length(); i++) {
+        // for (int j = 0; j <= word2.length(); j++) {
+        // System.out.println(dp[i][j]);
+        // }
+        // System.out.println();
+        // }
+
+        return dp[word1.length()][word2.length()];
     }
 }
 // @lc code=end
-
