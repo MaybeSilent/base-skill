649fe22f67f0f97b871853141a7cbdad4972b20a
MaybeSilent <804758962@qq.com>
2021-07-08 10:35:52 +0800

    add 73

--- leetcode/73.java
+++ leetcode/73.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+
+import org.graalvm.compiler.core.match.MatchRule;
+
 /*
  * @lc app=leetcode.cn id=73 lang=java
  *
@@ -55,8 +59,51 @@
 // @lc code=start
 class Solution {
     public void setZeroes(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0)
+            return;
 
+        boolean row = false, line = false;
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] == 0) {
+                row = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                line = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (row) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (line) {
+            Arrays.fill(matrix[0], 0);
+        }
+        return;
     }
 }
 // @lc code=end
-
