dc181bf344d07708a0178d82007abc4b5cf87626
MaybeSilent <804758962@qq.com>
2021-07-13 11:12:53 +0800

    add 83

--- leetcode/83.java
+++ leetcode/83.java
@@ -47,19 +47,24 @@
 
 // @lc code=start
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
+ * ListNode next) { this.val = val; this.next = next; } }
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
+        ListNode fast = head, slow = head;
 
+        while (fast != null) {
+            if (fast.val != slow.val) {
+                slow = slow.next;
+                slow.val = fast.val;
+            }
+            fast = fast.next;
+        }
+        if (slow != null)
+            slow.next = null;
+        return head;
     }
 }
 // @lc code=end
-
