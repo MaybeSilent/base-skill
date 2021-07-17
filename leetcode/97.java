6dc06d762517ac9c96b75188665d2f78680dffec
MaybeSilent <804758962@qq.com>
2021-07-17 11:39:07 +0800

    add 97,98

--- leetcode/97.java
+++ leetcode/97.java
@@ -1,3 +1,4 @@
+
 /*
  * @lc app=leetcode.cn id=97 lang=java
  *
@@ -65,8 +66,95 @@
 // @lc code=start
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
 
+        char[] s1Str = s1.toCharArray();
+        char[] s2Str = s2.toCharArray();
+        char[] s3Str = s3.toCharArray();
+
+        boolean[][] dp = new boolean[s1Str.length + 1][s2Str.length + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= s1Str.length; i++) {
+            dp[i][0] = dp[i - 1][0] && s1Str[i - 1] == s3Str[i - 1];
+        }
+
+        for (int i = 1; i <= s2Str.length; i++) {
+            dp[0][i] = dp[0][i - 1] && s2Str[i - 1] == s3Str[i - 1];
+        }
+
+        for (int i = 1; i <= s1Str.length; i++) {
+            for (int j = 1; j <= s2Str.length; j++) {
+                dp[i][j] = (dp[i - 1][j] && s1Str[i - 1] == s3Str[i + j - 1])
+                        || (dp[i][j - 1] && s2Str[j - 1] == s3Str[i + j - 1]);
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
+    }
+
+    public boolean TLE(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+
+        int[][] results = new int[s1.length() + 1][s2.length() + 1];
+        return isInterleaveChar(results, s1.toCharArray(), s2.toCharArray(), s3.toCharArray(), 0, 0, 0);
+    }
+
+    public boolean isInterleaveChar(int[][] results, char[] s1, char[] s2, char[] s3, int index1, int index2,
+            int index3) {
+        if (index3 >= s3.length) {
+            results[index1][index2] = 1;
+            return true;
+        }
+
+        if (results[index1][index2] != 0) {
+            return results[index1][index2] == 1;
+        }
+
+        while (index3 < s3.length) {
+            if (index1 >= s1.length || index2 >= s2.length) {
+                if (index1 >= s1.length) {
+                    for (int i = 0; i < s2.length - index2; i++) {
+                        if (s2[i + index2] != s3[i + index3]) {
+                            results[index1][index2] = -1;
+                            return false;
+                        }
+                    }
+                    results[index1][index2] = 1;
+                    return true;
+                }
+
+                if (index2 >= s2.length) {
+                    for (int i = 0; i < s1.length - index1; i++) {
+                        if (s1[i + index1] != s3[i + index3]) {
+                            results[index1][index2] = -1;
+                            return false;
+                        }
+                    }
+                    results[index1][index2] = 1;
+                    return true;
+                }
+            }
+
+            if (s1[index1] == s3[index3] && s2[index2] == s3[index3]) {
+                return isInterleaveChar(results, s1, s2, s3, index1 + 1, index2, index3 + 1)
+                        || isInterleaveChar(results, s1, s2, s3, index1, index2 + 1, index3 + 1);
+            } else if (s1[index1] == s3[index3]) {
+                index1++;
+                index3++;
+            } else if (s2[index2] == s3[index3]) {
+                index2++;
+                index3++;
+            } else {
+                results[index1][index2] = -1;
+                return false;
+            }
+        }
+        results[index1][index2] = 1;
+        return true;
     }
 }
 // @lc code=end
-
--- leetcode/98.java
+++ leetcode/98.java
@@ -49,24 +49,35 @@
 
 // @lc code=start
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
+        return isValidBSTWithValue(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
 
+    public boolean isValidBSTWithValue(TreeNode root, long minValue, long maxValue) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.left != null) {
+            if (root.left.val >= root.val || root.left.val <= minValue) {
+                return false;
+            }
+        }
+
+        if (root.right != null) {
+            if (root.right.val <= root.val || root.right.val >= maxValue) {
+                return false;
+            }
+        }
+
+        return isValidBSTWithValue(root.left, minValue, (long) root.val)
+                && isValidBSTWithValue(root.right, (long) root.val, maxValue);
     }
 }
 // @lc code=end
-
